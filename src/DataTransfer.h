8a2e772e964e99eb67716d4d20acc60493997960
michaelwithnospaces <michaelwithnospaces@proton.me>
2024-10-22 02:15:45 -0500

    feat: DataTransfer

---
+++ src/DataTransfer.h
@@ -0,0 +1,12 @@
+#ifndef DATA_TRANSFER_H
+#define DATA_TRANSFER_H
+
+class DataTransfer 
+{
+public:
+    DataTransfer();
+    void packDataToCSV(char* result, const char* data[], int length);
+    void unpackCSVData(const char* csvData, char* outputArray[], int& length);
+};
+
+#endif
